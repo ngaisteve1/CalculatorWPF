e64f2e69b343849d5c8a1149cb0962eb3d0a7732
Ngai Chee Weng <findingsteve@hotmail.com>
2019-05-28 18:27:09 +0800

    Initial

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double lastNumber;
+        string lastNumberString;        
+        lastInputType lastInputType;
+        bool firstZero;
+
+        private void Init()
+        {
+            firstZero = false;
+            resultLabelExp.Content = "";
+            lastNumberString = "";
+            lastInputType = lastInputType.Operator;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            acButton.Click += AcButton_Click;
+            negativeButton.Click += NegativeButton_Click;
+            percentageButton.Click += PercentageButton_Click;
+            equalButton.Click += EqualButton_Click;
+
+            Init();
+        }
+
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = MathBodmas.EvalExpression(resultLabelExp.Content.ToString().ToCharArray()).ToString();
+            Init();
+        }
+
+        private void PercentageButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber / 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void AcButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultLabelExp.Content = "";
+            resultLabel.Content = "0";
+            Init();
+        }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var operatorBtn = sender as Button;
+            resultLabelExp.Content = $"{resultLabelExp.Content}{operatorBtn.Content.ToString()}";
+            lastInputType = lastInputType.Operator;
+            lastNumber = 0;
+            lastNumberString = "";
+            firstZero = false;
+        }
+
+        private void pointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString().Contains("."))
+            {
+                // Do nothing
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            var numberBtn = sender as Button;
+
+            var selectedValue = numberBtn.Content.ToString();
+
+            switch (lastInputType)
+            {
+                case lastInputType.Operator:
+                case lastInputType.Zero:
+                    if(lastNumberString == "0")
+                    {
+
+                        // Special checking for the first lastNumberString
+                        if(firstZero)
+                        {
+                            // Replace
+                            lastNumberString = selectedValue;
+
+                            // Extract whole string without last char using substring.
+                            resultLabelExp.Content = resultLabelExp.Content.ToString().Substring(0, resultLabelExp.Content.ToString().Length - 1);
+
+                            resultLabelExp.Content += lastNumberString;
+                            
+                            
+                            firstZero = false;
+                        }
+                        else
+                        {
+                            // Append
+                            resultLabelExp.Content += lastNumberString;
+                        }
+                        
+                    }
+                    else
+                    {
+                        // Append
+                        lastNumberString += selectedValue;
+                        resultLabelExp.Content += selectedValue;
+                    }
+
+                    break;
+                case lastInputType.Number:
+                    // Append
+                    lastNumber += Convert.ToDouble(selectedValue);
+                    resultLabelExp.Content = $"{resultLabelExp.Content}{selectedValue}";
+                    lastInputType = lastInputType.Number;
+                    break;
+                
+                
+                default:
+                    break;
+            }
+
+        }
+
+        private void ZeroButton_Click(object sender, RoutedEventArgs e)
+        {
+            var zeroButton = sender as Button;
+            var selectedValue = zeroButton.Content.ToString();
+            
+            // If lastNumber is firstZero, do nothing
+            if (lastNumberString == "")
+            {
+                lastNumberString = "0";
+                resultLabelExp.Content += 0.ToString();
+                firstZero = true;
+                // Do nothing
+            }
+            else if (lastNumberString.Length == 1 && lastNumberString == "0"){
+                // Do nothing
+            }
+            else
+            {
+                switch (lastInputType)
+                {
+                    case lastInputType.Zero:
+                        //// Append
+                        resultLabelExp.Content += 0.ToString();
+                        //firstZero = true;
+                        break;
+                    case lastInputType.Number:
+                        lastNumberString += "0";
+                        // Append
+                        resultLabelExp.Content += selectedValue.ToString();
+                        break;
+                    case lastInputType.Operator:
+                        // Append
+                        resultLabelExp.Content += selectedValue.ToString();
+                        break;
+                    default:
+                        break;
+                }
+                
+            }
+
+            lastInputType = lastInputType.Zero;
+        }
+    }
+
+    public enum lastInputType
+    {
+        Zero, Number, Operator
+    }
+}
---
+++ Calculator/MathBodmas.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class MathBodmas
+    {
+        //  Implementation
+        //  string exp = "2*2+1";
+        //  Console.WriteLine(MathParser.EvalExpression(exp.ToCharArray()).ToString());
+        public static double EvalExpression(char[] expr)
+        {
+            return parseSummands(expr, 0);
+        }
+
+        private static double parseSummands(char[] expr, int index)
+        {
+            // index is passed as reference to maintain one copy throughout
+            double x = parseFactors(expr, ref index);
+            while (true)
+            {
+                char op = expr[index];
+                if (op != '+' && op != '-')
+                    return x;
+
+                index++;
+                double y = parseFactors(expr, ref index);
+                if (op == '+')
+                    x += y;
+                else
+                    x -= y;
+            }
+        }
+
+        private static double parseFactors(char[] expr, ref int index)
+        {
+            double x = GetDouble(expr, ref index);
+            while (true)
+            {
+                char op = expr[index];
+                if (op != '/' && op != '*')
+                    return x;
+
+                index++;
+                double y = GetDouble(expr, ref index);
+                if (op == '/')
+                    x /= y;
+                else
+                    x *= y;
+            }
+        }
+
+        private static double GetDouble(char[] expr, ref int index)
+        {
+            string dbl = "";
+
+            //ASCII table:
+            //46 . 2E
+            //48 0 30
+            //49 1 31
+            //50 2 32
+            //51 3 33
+            //52 4 34
+            //53 5 35
+            //54 6 36
+            //55 7 37
+            //56 8 38
+            //57 9 39
+            while (((int)expr[index] >= 48 && (int)expr[index] <= 57) || expr[index] == 46)
+            {
+                dbl = dbl + expr[index].ToString();
+                index++;
+                if (index == expr.Length)
+                {
+                    index--;
+                    break;
+                }
+            }
+            return double.Parse(dbl);
+        }
+    }
+}
